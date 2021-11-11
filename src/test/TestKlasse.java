1aecba8649fdeced51d0abd57254c804bceb35bf
Maulesel42 <91535093+Maulesel42@users.noreply.github.com>
2021-11-11 15:16:09 +0100

    Regeln

--- src/test/TestKlasse.java
+++ src/test/TestKlasse.java
@@ -1,9 +1,35 @@
 package test;
 
+/**
+ * Das ist die Klasse TestKlasse für unser Spiel Slide.
+ * 
+ * @author Angelina
+ * @version 1.0
+ *
+ */
 public class TestKlasse {
-
+	/**
+	 * Hier wird der String "Test" ausgegeben.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// einzeiliger Kommentar
 		System.out.print("Test");
 	}
 
 }
+/**
+ * 
+ * 
+ * Hier noch ein paar Tipps fuer guten Code:
+ * - Methodennamen beginnen mit Kleinbuchstaben
+ * - Variablennamen beginnen mit Kleinbuchstaben
+ * - Klassennamen beginnen mit Großbuchstaben
+ * - Pro Zeile genau eine Deklaration bzw. genau ein Statement
+ * - Minimale Sichtbarkeit von Methoden und Feldern
+ * - Methoden und Variablen bekommen aussagekraeftige Namen
+ * - alles mit JavaDoc kommentieren
+ * 
+ *
+ */
