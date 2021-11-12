d3b5d52d92b6a42877d4de1bcf44d0f64f09d085
Nico <Nico@THINK-Nico.fritz.box>
2021-11-12 16:33:00 +0100

    Methoden in Klasse Spiel hinzugefügt

--- src/slide/Spiel.java
+++ src/slide/Spiel.java
@@ -2,10 +2,26 @@ package slide;
 
 /**
  * 
- * @author Paul
+ * @author Nico
  * @version 0.1
  *
  */
 public class Spiel {
 
+	public void spielerHinzufügen(String name) {
+		
+	}
+	
+	public void einwerfen(String richtung, int reihe) {
+		
+	}
+	
+	public void bombeSetzen(int x, int y) {		//x=Spalte und y=Reihe, Koordinatensystem
+		
+	}
+
+	public boolean spielEnde() {
+		return false;
+	}
+	
 }
