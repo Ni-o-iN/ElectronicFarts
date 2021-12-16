82ba3c4bf71925a7e6b411999dfb43171cb58f67
Nico <Nico@THINK-Nico.fritz.box>
2021-12-16 23:02:13 +0100

    KI angefangen

---
+++ src/slide/AI.java
@@ -0,0 +1,83 @@
+package slide;
+
+import java.util.Random;
+
+public class AI extends GameLogic {
+
+	Random random = new Random();
+	
+	final int ROW = 6;
+	final int COLUMN = 7;
+	private char[][] field = new char[ROW][COLUMN];
+	
+	//TODO Spielfeld kopieren
+//	AI() {
+//		for (int z = 0; z < field.length; z++) {
+//			for (int s = 0; s < field[z].length; s++) {
+//				field[z][s] = Board.getSignFromField(z, s);
+//			}
+//		}
+//	}
+	
+	
+	public void randomDirection() {
+		int direction = random.nextInt(4);
+		if(direction == 1) {
+			top();
+		} else if(direction == 2) {
+			right();
+		}else if (direction == 3) {
+			bottom();
+		}else
+			left();
+	}
+	
+	public void top() {
+		int randomRow = random.nextInt(6);
+		if(isValidMove(randomRow, 1)) {
+			
+		}else top();
+	}
+	
+	public void right() {
+		int randomCollum = random.nextInt(7);
+		if(isValidMove(1, randomCollum)) {
+			
+		}
+	}
+
+	public void bottom() {
+		int randomRow = random.nextInt(6);
+		if(isValidMove(randomRow, 1)) {
+			
+		}
+	}
+
+	public void left() {
+		int randomCollum = random.nextInt(7);
+		if(isValidMove(1, randomCollum)) {
+			
+		}
+	}
+	
+	public void losePossibility() {
+		
+	}
+	
+	public void winPossibility() {
+		
+	}
+	
+	public void tokenMoveEasy() {
+		
+	}
+	
+	public void tokenMoveNormal() {
+		
+	}
+
+	public void tokenMoveHard() {
+	
+	}
+	
+}
